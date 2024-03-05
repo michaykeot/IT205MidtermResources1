e3a05d3c7d6d2ad160e5e0a56b5abc1c5ac442b8
michaykeot <157451944+michaykeot@users.noreply.github.com>
2024-03-05 15:44:24 +0800

    Add files via upload

---
+++ input.py
@@ -0,0 +1,8 @@
+num1 = int (input("Enter 1st number: ")) #input() has data type
+int(num1) #int() for converting string to integer
+num2 = int (input("Enter 2nd number: "))
+int(num2)
+
+sum = num1 + num2
+
+print(f"The sum of {num1} and {num2} is: {sum}")
---
+++ output.py
@@ -0,0 +1,12 @@
+x = 20
+message = "HI PO! "
+ratio = 3.14
+
+#checking the type of the data with type() method.
+print(type(message))
+print(type(ratio))
+print(type(x))
+
+#string formatting
+print(message + "my name is michelle. I am " + str(x) + " years old.")
+print(f"{message} Michelle! You are  {x} years old.")
